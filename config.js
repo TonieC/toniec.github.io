ec82d5d06dd1813644e524b363f568624e436ffe
TonieC <jesstonievlogs@gmail.com>
2025-07-26 06:48:26 +0800

    Testing new blog

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+  title: "Tonie's Public Posts",
+  author: "Tonie",
+  repo: "https://github.com/TonieC/toniec.github.io", 
+  postsFolder: "posts/",
+  postExtension: ".md",
+  fadeSpeed: 300,
+  formatDates: true,
+  sortDate: "desc"
+};
